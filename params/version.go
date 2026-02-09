4733f0071da72b61671660426290cda6b573ec56
Nyxen <1020080523@qq.com>
2026-02-09 02:56:42 -0500

    build: auto-increment build number on every compilation
    - Add version-bump dependency to both `build` and `n42` Makefile targets
    - Sync VERSION file with params/version.go (was out of date at 5.1.487)
    - Build number now auto-increments before each compile (523 -> 524 -> ...)

--- params/version.go
+++ params/version.go
@@ -36,7 +36,7 @@ var (
 const (
 	VersionMajor       = 5   // Major version - annual release
 	VersionMinor       = 3   // Minor version - feature release
-	VersionBuild       = 523 // Build number - auto-incremented
+	VersionBuild       = 524 // Build number - auto-incremented
 	VersionModifier    = ""  // Modifier component (alpha, beta, stable)
 	VersionKeyCreated  = "n42VersionCreated"
 	VersionKeyFinished = "n42VersionFinished"
