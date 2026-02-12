6aa963fbe46de34e2182aaa3ef72e769df78b6e1
Nyxen <1020080523@qq.com>
2026-02-12 18:49:09 -0500

    release: bump version to v5.4.604

--- params/version.go
+++ params/version.go
@@ -36,7 +36,7 @@ var (
 const (
 	VersionMajor       = 5   // Major version - annual release
 	VersionMinor       = 4   // Minor version - feature release
-	VersionBuild       = 600 // Build number - auto-incremented
+	VersionBuild       = 604 // Build number - auto-incremented
 	VersionModifier    = ""  // Modifier component (alpha, beta, stable)
 	VersionKeyCreated  = "n42VersionCreated"
 	VersionKeyFinished = "n42VersionFinished"
