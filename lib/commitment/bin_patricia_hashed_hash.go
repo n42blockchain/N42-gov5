fd6b1f30e0e39512fd2e9c7430ea53104c9a53de
Nyxen <40690755+MiraWells@users.noreply.github.com>
2026-02-26 18:40:33 -0500

    refactor(commitment): split hex_patricia_hashed and bin_patricia_hashed into focused files
    Split hex_patricia_hashed.go (1982 lines) into 4 files:
    - hex_patricia_hashed_types.go: type definitions, constants, Update codec
    - hex_patricia_hashed_hash.go: hash computation methods
    - hex_patricia_hashed_ops.go: cell CRUD operations
    - hex_patricia_hashed.go: tree traversal, fold/unfold, state encode/decode
    
    Split bin_patricia_hashed.go (1818 lines) into 4 files:
    - bin_patricia_hashed_types.go: type definitions, constants, conversion utils
    - bin_patricia_hashed_hash.go: hash computation methods
    - bin_patricia_hashed_ops.go: cell CRUD operations
    - bin_patricia_hashed.go: tree traversal, fold/unfold, state encode/decode
    
    No behavioral changes; all exports preserved.

---
+++ lib/commitment/bin_patricia_hashed_hash.go
@@ -0,0 +1,413 @@
+/*
+   Copyright 2022 The Erigon contributors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package commitment
+
+import (
+	"fmt"
+	"io"
+	"math/bits"
+
+	"github.com/n42blockchain/N42/lib/common"
+	"github.com/n42blockchain/N42/lib/common/length"
+	"github.com/n42blockchain/N42/lib/rlp"
+)
+
+func binHashKey(keccak keccakState, plainKey []byte, dest []byte, hashedKeyOffset int) error {
+	keccak.Reset()
+	var hashBufBack [length.Hash]byte
+	hashBuf := hashBufBack[:]
+	if _, err := keccak.Write(plainKey); err != nil {
+		return err
+	}
+	if _, err := keccak.Read(hashBuf); err != nil {
+		return err
+	}
+	for k := hashedKeyOffset; k < 256; k++ {
+		if hashBuf[k/8]&(1<<(7-k%8)) == 0 {
+			dest[k-hashedKeyOffset] = 0
+		} else {
+			dest[k-hashedKeyOffset] = 1
+		}
+	}
+	return nil
+}
+
+func (cell *BinaryCell) accountForHashing(buffer []byte, storageRootHash [length.Hash]byte) int {
+	balanceBytes := 0
+	if !cell.Balance.LtUint64(128) {
+		balanceBytes = cell.Balance.ByteLen()
+	}
+
+	var nonceBytes int
+	if cell.Nonce < 128 && cell.Nonce != 0 {
+		nonceBytes = 0
+	} else {
+		nonceBytes = common.BitLenToByteLen(bits.Len64(cell.Nonce))
+	}
+
+	var structLength = uint(balanceBytes + nonceBytes + 2)
+	structLength += 66 // Two 32-byte arrays + 2 prefixes
+
+	var pos int
+	if structLength < 56 {
+		buffer[0] = byte(192 + structLength)
+		pos = 1
+	} else {
+		lengthBytes := common.BitLenToByteLen(bits.Len(structLength))
+		buffer[0] = byte(247 + lengthBytes)
+
+		for i := lengthBytes; i > 0; i-- {
+			buffer[i] = byte(structLength)
+			structLength >>= 8
+		}
+
+		pos = lengthBytes + 1
+	}
+
+	// Encoding nonce
+	if cell.Nonce < 128 && cell.Nonce != 0 {
+		buffer[pos] = byte(cell.Nonce)
+	} else {
+		buffer[pos] = byte(128 + nonceBytes)
+		var nonce = cell.Nonce
+		for i := nonceBytes; i > 0; i-- {
+			buffer[pos+i] = byte(nonce)
+			nonce >>= 8
+		}
+	}
+	pos += 1 + nonceBytes
+
+	// Encoding balance
+	if cell.Balance.LtUint64(128) && !cell.Balance.IsZero() {
+		buffer[pos] = byte(cell.Balance.Uint64())
+		pos++
+	} else {
+		buffer[pos] = byte(128 + balanceBytes)
+		pos++
+		cell.Balance.WriteToSlice(buffer[pos : pos+balanceBytes])
+		pos += balanceBytes
+	}
+
+	// Encoding Root and CodeHash
+	buffer[pos] = 128 + 32
+	pos++
+	copy(buffer[pos:], storageRootHash[:])
+	pos += 32
+	buffer[pos] = 128 + 32
+	pos++
+	copy(buffer[pos:], cell.CodeHash[:])
+	pos += 32
+	return pos
+}
+
+func (bph *BinPatriciaHashed) completeLeafHash(buf, keyPrefix []byte, kp, kl, compactLen int, key []byte, compact0 byte, ni int, val rlp.RlpSerializable, singleton bool) ([]byte, error) {
+	totalLen := kp + kl + val.DoubleRLPLen()
+	var lenPrefix [4]byte
+	pt := rlp.GenerateStructLen(lenPrefix[:], totalLen)
+	embedded := !singleton && totalLen+pt < length.Hash
+	var writer io.Writer
+	if embedded {
+		bph.auxBuffer.Reset()
+		writer = bph.auxBuffer
+	} else {
+		bph.keccak.Reset()
+		writer = bph.keccak
+	}
+	if _, err := writer.Write(lenPrefix[:pt]); err != nil {
+		return nil, err
+	}
+	if _, err := writer.Write(keyPrefix[:kp]); err != nil {
+		return nil, err
+	}
+	var b [1]byte
+	b[0] = compact0
+	if _, err := writer.Write(b[:]); err != nil {
+		return nil, err
+	}
+	for i := 1; i < compactLen; i++ {
+		b[0] = key[ni]*16 + key[ni+1]
+		if _, err := writer.Write(b[:]); err != nil {
+			return nil, err
+		}
+		ni += 2
+	}
+	var prefixBuf [8]byte
+	if err := val.ToDoubleRLP(writer, prefixBuf[:]); err != nil {
+		return nil, err
+	}
+	if embedded {
+		buf = bph.auxBuffer.Bytes()
+	} else {
+		var hashBuf [33]byte
+		hashBuf[0] = 0x80 + length.Hash
+		if _, err := bph.keccak.Read(hashBuf[1:]); err != nil {
+			return nil, err
+		}
+		buf = append(buf, hashBuf[:]...)
+	}
+	return buf, nil
+}
+
+func (bph *BinPatriciaHashed) leafHashWithKeyVal(buf, key []byte, val rlp.RlpSerializableBytes, singleton bool) ([]byte, error) {
+	var kp, kl int
+	var compactLen int
+	var ni int
+	var compact0 byte
+	compactLen = (len(key)-1)/2 + 1
+	if len(key)&1 == 0 {
+		compact0 = 0x30 + key[0] // Odd: (3<<4) + first nibble
+		ni = 1
+	} else {
+		compact0 = 0x20
+	}
+	var keyPrefix [1]byte
+	if compactLen > 1 {
+		keyPrefix[0] = 0x80 + byte(compactLen)
+		kp = 1
+		kl = compactLen
+	} else {
+		kl = 1
+	}
+	return bph.completeLeafHash(buf, keyPrefix[:], kp, kl, compactLen, key, compact0, ni, val, singleton)
+}
+
+func (bph *BinPatriciaHashed) accountLeafHashWithKey(buf, key []byte, val rlp.RlpSerializable) ([]byte, error) {
+	var kp, kl int
+	var compactLen int
+	var ni int
+	var compact0 byte
+	if hasTerm(key) {
+		compactLen = (len(key)-1)/2 + 1
+		if len(key)&1 == 0 {
+			compact0 = 48 + key[0] // Odd (1<<4) + first nibble
+			ni = 1
+		} else {
+			compact0 = 32
+		}
+	} else {
+		compactLen = len(key)/2 + 1
+		if len(key)&1 == 1 {
+			compact0 = 16 + key[0] // Odd (1<<4) + first nibble
+			ni = 1
+		}
+	}
+	var keyPrefix [1]byte
+	if compactLen > 1 {
+		keyPrefix[0] = byte(128 + compactLen)
+		kp = 1
+		kl = compactLen
+	} else {
+		kl = 1
+	}
+	return bph.completeLeafHash(buf, keyPrefix[:], kp, kl, compactLen, key, compact0, ni, val, true)
+}
+
+func (bph *BinPatriciaHashed) extensionHash(key []byte, hash []byte) ([length.Hash]byte, error) {
+	var hashBuf [length.Hash]byte
+
+	var kp, kl int
+	var compactLen int
+	var ni int
+	var compact0 byte
+	if hasTerm(key) {
+		compactLen = (len(key)-1)/2 + 1
+		if len(key)&1 == 0 {
+			compact0 = 0x30 + key[0] // Odd: (3<<4) + first nibble
+			ni = 1
+		} else {
+			compact0 = 0x20
+		}
+	} else {
+		compactLen = len(key)/2 + 1
+		if len(key)&1 == 1 {
+			compact0 = 0x10 + key[0] // Odd: (1<<4) + first nibble
+			ni = 1
+		}
+	}
+	var keyPrefix [1]byte
+	if compactLen > 1 {
+		keyPrefix[0] = 0x80 + byte(compactLen)
+		kp = 1
+		kl = compactLen
+	} else {
+		kl = 1
+	}
+	totalLen := kp + kl + 33
+	var lenPrefix [4]byte
+	pt := rlp.GenerateStructLen(lenPrefix[:], totalLen)
+	bph.keccak.Reset()
+	if _, err := bph.keccak.Write(lenPrefix[:pt]); err != nil {
+		return hashBuf, err
+	}
+	if _, err := bph.keccak.Write(keyPrefix[:kp]); err != nil {
+		return hashBuf, err
+	}
+	var b [1]byte
+	b[0] = compact0
+	if _, err := bph.keccak.Write(b[:]); err != nil {
+		return hashBuf, err
+	}
+	for i := 1; i < compactLen; i++ {
+		b[0] = key[ni]*16 + key[ni+1]
+		if _, err := bph.keccak.Write(b[:]); err != nil {
+			return hashBuf, err
+		}
+		ni += 2
+	}
+	b[0] = 0x80 + length.Hash
+	if _, err := bph.keccak.Write(b[:]); err != nil {
+		return hashBuf, err
+	}
+	if _, err := bph.keccak.Write(hash); err != nil {
+		return hashBuf, err
+	}
+	// Replace previous hash with the new one
+	if _, err := bph.keccak.Read(hashBuf[:]); err != nil {
+		return hashBuf, err
+	}
+	return hashBuf, nil
+}
+
+func (bph *BinPatriciaHashed) computeBinaryCellHashLen(cell *BinaryCell, depth int) int {
+	if cell.spl > 0 && depth >= halfKeySize {
+		keyLen := 128 - depth + 1 // Length of hex key with terminator character
+		var kp, kl int
+		compactLen := (keyLen-1)/2 + 1
+		if compactLen > 1 {
+			kp = 1
+			kl = compactLen
+		} else {
+			kl = 1
+		}
+		val := rlp.RlpSerializableBytes(cell.Storage[:cell.StorageLen])
+		totalLen := kp + kl + val.DoubleRLPLen()
+		var lenPrefix [4]byte
+		pt := rlp.GenerateStructLen(lenPrefix[:], totalLen)
+		if totalLen+pt < length.Hash {
+			return totalLen + pt
+		}
+	}
+	return length.Hash + 1
+}
+
+func (bph *BinPatriciaHashed) computeBinaryCellHash(cell *BinaryCell, depth int, buf []byte) ([]byte, error) {
+	var err error
+	var storageRootHash [length.Hash]byte
+	storageRootHashIsSet := false
+	if cell.spl > 0 {
+		var hashedKeyOffset int
+		if depth >= halfKeySize {
+			hashedKeyOffset = depth - halfKeySize
+		}
+		singleton := depth <= halfKeySize
+		if err := binHashKey(bph.keccak, cell.spk[bph.accountKeyLen:cell.spl], cell.downHashedKey[:], hashedKeyOffset); err != nil {
+			return nil, err
+		}
+		cell.downHashedKey[halfKeySize-hashedKeyOffset] = 16 // Add terminator
+		if singleton {
+			if bph.trace {
+				fmt.Printf("leafHashWithKeyVal(singleton) for [%x]=>[%x]\n", cell.downHashedKey[:halfKeySize-hashedKeyOffset+1], cell.Storage[:cell.StorageLen])
+			}
+			aux := make([]byte, 0, 33)
+			if aux, err = bph.leafHashWithKeyVal(aux, cell.downHashedKey[:halfKeySize-hashedKeyOffset+1], cell.Storage[:cell.StorageLen], true); err != nil {
+				return nil, err
+			}
+			storageRootHash = *(*[length.Hash]byte)(aux[1:])
+			storageRootHashIsSet = true
+		} else {
+			if bph.trace {
+				fmt.Printf("leafHashWithKeyVal for [%x]=>[%x]\n", cell.downHashedKey[:halfKeySize-hashedKeyOffset+1], cell.Storage[:cell.StorageLen])
+			}
+			return bph.leafHashWithKeyVal(buf, cell.downHashedKey[:halfKeySize-hashedKeyOffset+1], cell.Storage[:cell.StorageLen], false)
+		}
+	}
+	if cell.apl > 0 {
+		if err := binHashKey(bph.keccak, cell.apk[:cell.apl], cell.downHashedKey[:], depth); err != nil {
+			return nil, err
+		}
+		cell.downHashedKey[halfKeySize-depth] = 16 // Add terminator
+		if !storageRootHashIsSet {
+			if cell.extLen > 0 {
+				// Extension
+				if cell.hl > 0 {
+					if bph.trace {
+						fmt.Printf("extensionHash for [%x]=>[%x]\n", cell.extension[:cell.extLen], cell.h[:cell.hl])
+					}
+					if storageRootHash, err = bph.extensionHash(cell.extension[:cell.extLen], cell.h[:cell.hl]); err != nil {
+						return nil, err
+					}
+				} else {
+					return nil, fmt.Errorf("computeBinaryCellHash extension without hash")
+				}
+			} else if cell.hl > 0 {
+				storageRootHash = cell.h
+			} else {
+				storageRootHash = *(*[length.Hash]byte)(EmptyRootHash)
+			}
+		}
+		var valBuf [128]byte
+		valLen := cell.accountForHashing(valBuf[:], storageRootHash)
+		if bph.trace {
+			fmt.Printf("accountLeafHashWithKey for [%x]=>[%x]\n", bph.hashAuxBuffer[:halfKeySize+1-depth], valBuf[:valLen])
+		}
+		return bph.accountLeafHashWithKey(buf, cell.downHashedKey[:halfKeySize+1-depth], rlp.RlpEncodedBytes(valBuf[:valLen]))
+	}
+	buf = append(buf, 0x80+32)
+	if cell.extLen > 0 {
+		// Extension
+		if cell.hl > 0 {
+			if bph.trace {
+				fmt.Printf("extensionHash for [%x]=>[%x]\n", cell.extension[:cell.extLen], cell.h[:cell.hl])
+			}
+			var hash [length.Hash]byte
+			if hash, err = bph.extensionHash(cell.extension[:cell.extLen], cell.h[:cell.hl]); err != nil {
+				return nil, err
+			}
+			buf = append(buf, hash[:]...)
+		} else {
+			return nil, fmt.Errorf("computeBinaryCellHash extension without hash")
+		}
+	} else if cell.hl > 0 {
+		buf = append(buf, cell.h[:cell.hl]...)
+	} else {
+		buf = append(buf, EmptyRootHash...)
+	}
+	return buf, nil
+}
+
+// Hashes provided key and expands resulting hash into nibbles (each byte split into two nibbles by 4 bits)
+func (bph *BinPatriciaHashed) hashAndNibblizeKey2(key []byte) []byte { //nolint
+	hashedKey := make([]byte, length.Hash)
+
+	bph.keccak.Reset()
+	bph.keccak.Write(key[:length.Addr])
+	copy(hashedKey[:length.Hash], bph.keccak.Sum(nil))
+
+	if len(key[length.Addr:]) > 0 {
+		hashedKey = append(hashedKey, make([]byte, length.Hash)...)
+		bph.keccak.Reset()
+		bph.keccak.Write(key[length.Addr:])
+		copy(hashedKey[length.Hash:], bph.keccak.Sum(nil))
+	}
+
+	nibblized := make([]byte, len(hashedKey)*2)
+	for i, b := range hashedKey {
+		nibblized[i*2] = (b >> 4) & 0xf
+		nibblized[i*2+1] = b & 0xf
+	}
+	return nibblized
+}
---
+++ lib/commitment/hex_patricia_hashed_types.go
@@ -0,0 +1,299 @@
+/*
+   Copyright 2022 The Erigon contributors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package commitment
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"hash"
+	"strings"
+
+	"github.com/holiman/uint256"
+	"golang.org/x/crypto/sha3"
+
+	"github.com/n42blockchain/N42/lib/common/length"
+)
+
+// keccakState wraps sha3.state. In addition to the usual hash methods, it also supports
+// Read to get a variable amount of data from the hash state. Read is faster than Sum
+// because it doesn't copy the internal state, but also modifies the internal state.
+type keccakState interface {
+	hash.Hash
+	Read([]byte) (int, error)
+}
+
+// HexPatriciaHashed implements commitment based on patricia merkle tree with radix 16,
+// with keys pre-hashed by keccak256
+type HexPatriciaHashed struct {
+	root Cell // Root cell of the tree
+	// How many rows (starting from row 0) are currently active and have corresponding selected columns
+	// Last active row does not have selected column
+	activeRows int
+	// Length of the key that reflects current positioning of the grid. It maybe larger than number of active rows,
+	// if an account leaf cell represents multiple nibbles in the key
+	currentKeyLen int
+	accountKeyLen int
+	// Rows of the grid correspond to the level of depth in the patricia tree
+	// Columns of the grid correspond to pointers to the nodes further from the root
+	grid         [128][16]Cell // First 64 rows of this grid are for account trie, and next 64 rows are for storage trie
+	currentKey   [128]byte     // For each row indicates which column is currently selected
+	depths       [128]int      // For each row, the depth of cells in that row
+	branchBefore [128]bool     // For each row, whether there was a branch node in the database loaded in unfold
+	touchMap     [128]uint16   // For each row, bitmap of cells that were either present before modification, or modified or deleted
+	afterMap     [128]uint16   // For each row, bitmap of cells that were present after modification
+	keccak       keccakState
+	keccak2      keccakState
+	rootChecked  bool // Set to false if it is not known whether the root is empty, set to true if it is checked
+	rootTouched  bool
+	rootPresent  bool
+	trace        bool
+	// Function used to load branch node and fill up the cells
+	// For each cell, it sets the cell type, clears the modified flag, fills the hash,
+	// and for the extension, account, and leaf type, the `l` and `k`
+	branchFn func(prefix []byte) ([]byte, error)
+	// Function used to fetch account with given plain key
+	accountFn func(plainKey []byte, cell *Cell) error
+	// Function used to fetch storage with given plain key
+	storageFn func(plainKey []byte, cell *Cell) error
+
+	hashAuxBuffer [128]byte     // buffer to compute cell hash or write hash-related things
+	auxBuffer     *bytes.Buffer // auxiliary buffer used during branch updates encoding
+}
+
+func NewHexPatriciaHashed(accountKeyLen int,
+	branchFn func(prefix []byte) ([]byte, error),
+	accountFn func(plainKey []byte, cell *Cell) error,
+	storageFn func(plainKey []byte, cell *Cell) error,
+) *HexPatriciaHashed {
+	return &HexPatriciaHashed{
+		keccak:        sha3.NewLegacyKeccak256().(keccakState),
+		keccak2:       sha3.NewLegacyKeccak256().(keccakState),
+		accountKeyLen: accountKeyLen,
+		branchFn:      branchFn,
+		accountFn:     accountFn,
+		storageFn:     storageFn,
+		auxBuffer:     bytes.NewBuffer(make([]byte, 8192)),
+	}
+}
+
+// represents state of the tree
+type state struct {
+	Root          []byte      // encoded root cell
+	Depths        [128]int    // For each row, the depth of cells in that row
+	TouchMap      [128]uint16 // For each row, bitmap of cells that were either present before modification, or modified or deleted
+	AfterMap      [128]uint16 // For each row, bitmap of cells that were present after modification
+	BranchBefore  [128]bool   // For each row, whether there was a branch node in the database loaded in unfold
+	CurrentKey    [128]byte   // For each row indicates which column is currently selected
+	CurrentKeyLen int8
+	RootChecked   bool // Set to false if it is not known whether the root is empty, set to true if it is checked
+	RootTouched   bool
+	RootPresent   bool
+}
+
+type stateRootFlag int8
+
+var (
+	stateRootPresent stateRootFlag = 1
+	stateRootChecked stateRootFlag = 2
+	stateRootTouched stateRootFlag = 4
+)
+
+type Cell struct {
+	Balance       uint256.Int
+	Nonce         uint64
+	hl            int // Length of the hash (or embedded)
+	StorageLen    int
+	apl           int // length of account plain key
+	spl           int // length of the storage plain key
+	downHashedLen int
+	extLen        int
+	downHashedKey [128]byte
+	extension     [64]byte
+	spk           [length.Addr + length.Hash]byte // storage plain key
+	h             [length.Hash]byte               // cell hash
+	CodeHash      [length.Hash]byte               // hash of the bytecode
+	Storage       [length.Hash]byte
+	apk           [length.Addr]byte // account plain key
+	Delete        bool
+}
+
+var (
+	EmptyRootHash, _ = hex.DecodeString("56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421")
+	EmptyCodeHash, _ = hex.DecodeString("c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470")
+)
+
+type UpdateFlags uint8
+
+const (
+	CodeUpdate    UpdateFlags = 1
+	DeleteUpdate  UpdateFlags = 2
+	BalanceUpdate UpdateFlags = 4
+	NonceUpdate   UpdateFlags = 8
+	StorageUpdate UpdateFlags = 16
+)
+
+func (uf UpdateFlags) String() string {
+	var sb strings.Builder
+	if uf == DeleteUpdate {
+		sb.WriteString("Delete")
+	} else {
+		if uf&BalanceUpdate != 0 {
+			sb.WriteString("+Balance")
+		}
+		if uf&NonceUpdate != 0 {
+			sb.WriteString("+Nonce")
+		}
+		if uf&CodeUpdate != 0 {
+			sb.WriteString("+Code")
+		}
+		if uf&StorageUpdate != 0 {
+			sb.WriteString("+Storage")
+		}
+	}
+	return sb.String()
+}
+
+type Update struct {
+	Flags             UpdateFlags
+	Balance           uint256.Int
+	Nonce             uint64
+	CodeHashOrStorage [length.Hash]byte
+	ValLength         int
+}
+
+func (u *Update) DecodeForStorage(enc []byte) {
+	u.Nonce = 0
+	u.Balance.Clear()
+	copy(u.CodeHashOrStorage[:], EmptyCodeHash)
+
+	pos := 0
+	nonceBytes := int(enc[pos])
+	pos++
+	if nonceBytes > 0 {
+		u.Nonce = bytesToUint64(enc[pos : pos+nonceBytes])
+		pos += nonceBytes
+	}
+	balanceBytes := int(enc[pos])
+	pos++
+	if balanceBytes > 0 {
+		u.Balance.SetBytes(enc[pos : pos+balanceBytes])
+		pos += balanceBytes
+	}
+	codeHashBytes := int(enc[pos])
+	pos++
+	if codeHashBytes > 0 {
+		copy(u.CodeHashOrStorage[:], enc[pos:pos+codeHashBytes])
+	}
+}
+
+func (u *Update) Encode(buf []byte, numBuf []byte) []byte {
+	buf = append(buf, byte(u.Flags))
+	if u.Flags&BalanceUpdate != 0 {
+		buf = append(buf, byte(u.Balance.ByteLen()))
+		buf = append(buf, u.Balance.Bytes()...)
+	}
+	if u.Flags&NonceUpdate != 0 {
+		n := binary.PutUvarint(numBuf, u.Nonce)
+		buf = append(buf, numBuf[:n]...)
+	}
+	if u.Flags&CodeUpdate != 0 {
+		buf = append(buf, u.CodeHashOrStorage[:]...)
+	}
+	if u.Flags&StorageUpdate != 0 {
+		n := binary.PutUvarint(numBuf, uint64(u.ValLength))
+		buf = append(buf, numBuf[:n]...)
+		if u.ValLength > 0 {
+			buf = append(buf, u.CodeHashOrStorage[:u.ValLength]...)
+		}
+	}
+	return buf
+}
+
+func (u *Update) Decode(buf []byte, pos int) (int, error) {
+	if len(buf) < pos+1 {
+		return 0, fmt.Errorf("decode Update: buffer too small for flags")
+	}
+	u.Flags = UpdateFlags(buf[pos])
+	pos++
+	if u.Flags&BalanceUpdate != 0 {
+		if len(buf) < pos+1 {
+			return 0, fmt.Errorf("decode Update: buffer too small for balance len")
+		}
+		balanceLen := int(buf[pos])
+		pos++
+		if len(buf) < pos+balanceLen {
+			return 0, fmt.Errorf("decode Update: buffer too small for balance")
+		}
+		u.Balance.SetBytes(buf[pos : pos+balanceLen])
+		pos += balanceLen
+	}
+	if u.Flags&NonceUpdate != 0 {
+		var n int
+		u.Nonce, n = binary.Uvarint(buf[pos:])
+		if n == 0 {
+			return 0, fmt.Errorf("decode Update: buffer too small for nonce")
+		}
+		if n < 0 {
+			return 0, fmt.Errorf("decode Update: nonce overflow")
+		}
+		pos += n
+	}
+	if u.Flags&CodeUpdate != 0 {
+		if len(buf) < pos+32 {
+			return 0, fmt.Errorf("decode Update: buffer too small for codeHash")
+		}
+		copy(u.CodeHashOrStorage[:], buf[pos:pos+32])
+		pos += 32
+	}
+	if u.Flags&StorageUpdate != 0 {
+		l, n := binary.Uvarint(buf[pos:])
+		if n == 0 {
+			return 0, fmt.Errorf("decode Update: buffer too small for storage len")
+		}
+		if n < 0 {
+			return 0, fmt.Errorf("decode Update: storage lee overflow")
+		}
+		pos += n
+		if len(buf) < pos+int(l) {
+			return 0, fmt.Errorf("decode Update: buffer too small for storage")
+		}
+		u.ValLength = int(l)
+		copy(u.CodeHashOrStorage[:], buf[pos:pos+int(l)])
+		pos += int(l)
+	}
+	return pos, nil
+}
+
+func (u *Update) String() string {
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("Flags: [%s]", u.Flags))
+	if u.Flags&BalanceUpdate != 0 {
+		sb.WriteString(fmt.Sprintf(", Balance: [%d]", &u.Balance))
+	}
+	if u.Flags&NonceUpdate != 0 {
+		sb.WriteString(fmt.Sprintf(", Nonce: [%d]", u.Nonce))
+	}
+	if u.Flags&CodeUpdate != 0 {
+		sb.WriteString(fmt.Sprintf(", CodeHash: [%x]", u.CodeHashOrStorage))
+	}
+	if u.Flags&StorageUpdate != 0 {
+		sb.WriteString(fmt.Sprintf(", Storage: [%x]", u.CodeHashOrStorage[:u.ValLength]))
+	}
+	return sb.String()
+}
