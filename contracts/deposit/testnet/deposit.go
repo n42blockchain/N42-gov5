62ff944557d81d8eafa12d1d1a45582b1a0afca2
Nyxen <1020080523@qq.com>
2026-03-21 04:08:27 -0400

    refactor: rename deposit packages to generic names + deprecate rlp2
    Deposit contract directories renamed from project-specific to universal names:
    - contracts/deposit/amt/ → contracts/deposit/token/ (fungible token staking)
    - contracts/deposit/fuji/ → contracts/deposit/testnet/ (testnet staking variant)
    - contracts/deposit/nft/ → contracts/deposit/nftstake/ (NFT-based staking)
    
    Package declarations updated:
    - amtdeposit → token, fujideposit → testnet, nftdeposit → nftstake
    - Go files renamed: amt.go/fuji.go/nft.go → deposit.go (matches package purpose)
    
    lib/rlp2: added deprecation notice (legacy, only used in 2 test files)

--- contracts/deposit/testnet/deposit.go
+++ contracts/deposit/testnet/deposit.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the N42 library. If not, see <http://www.gnu.org/licenses/>.
 
-package amtdeposit
+package testnet
 
 import (
 	"bytes"
