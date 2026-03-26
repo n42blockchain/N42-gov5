59ab91513fcdb3f0afabfed4048e967170dd66d3
Nyxen <1020080523@qq.com>
2026-03-26 16:38:13 -0400

    refactor(state): introduce transition chain policy

--- params/version.go
+++ params/version.go
@@ -37,7 +37,7 @@ var (
 const (
 	VersionMajor       = 5   // Major version - annual release
 	VersionMinor       = 6   // Minor version - feature release
-	VersionBuild       = 780 // Build number - auto-incremented
+	VersionBuild       = 781 // Build number - auto-incremented
 	VersionModifier    = ""  // Modifier component (alpha, beta, stable)
 	VersionKeyCreated  = "n42VersionCreated"
 	VersionKeyFinished = "n42VersionFinished"
