3aa5656fa11cd9c13dbcc9cde260207e1c6885e6
Nyxen <1020080523@qq.com>
2026-07-19 01:25:31 -0400

    chore: bump version to 5.7.891

--- params/version.go
+++ params/version.go
@@ -43,7 +43,7 @@ var (
 const (
 	VersionMajor       = 5   // Major version - annual release
 	VersionMinor       = 7   // Minor version - feature release
-	VersionBuild       = 890 // Build number - auto-incremented
+	VersionBuild       = 891 // Build number - auto-incremented
 	VersionModifier    = ""  // Modifier component (alpha, beta, stable)
 	VersionKeyCreated  = "n42VersionCreated"
 	VersionKeyFinished = "n42VersionFinished"
